3c9f3ae04cdafcef5b37725fae3c19a349b3201f
Matheus <mtguerson@gmail.com>
2024-11-29 21:20:02 -0400

    feat: ensure transactions and subscription pages are protected

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,5 +1,13 @@
+import { auth } from "@clerk/nextjs/server";
 import NavBar from "../_components/navbar";
+import { redirect } from "next/navigation";
 
 export default function SubscriptionPage() {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
+
   return <NavBar />;
 }
